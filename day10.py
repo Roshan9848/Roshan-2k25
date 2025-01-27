02d98453a92f0cb2fc09a1bbdd6c41eca2a74e19
Roshan9848 <152873275+Roshan9848@users.noreply.github.com>
2025-01-27 07:18:24 +0000

    day13/2

--- day10.py
+++ day10.py
@@ -8,13 +8,6 @@ plt.show()
 
 #2
 
-import pandas as pd 
-import matplotlib.pyplot as plt
-import seaborn as sns
-datf = pd.DataFrame({"Season 1": [7,4,5,6,3],
-"Season 2": [1,2,8,4,9]})
-p = sns.histplot(data = datf)
-p.set(xlabel="X Label value",ylabel = "Y Label Value")
-plt.show()
+
 
 #3
